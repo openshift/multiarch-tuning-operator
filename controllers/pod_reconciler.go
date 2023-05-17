8c208b70b99e54f9121ad955258213f7a7d6ca30
aleskandro <aleskandro@redhat.com>
2023-05-17 16:22:29 +0200

    Implement the image inspection and the fulfilling of a supportedArchitectures set
    The current implementation just addresses the case of images accessible publicly in reachable registries. Also, it could be ideal to change this implementation so that windows containers are taken into account too.

--- controllers/pod_reconciler.go
+++ controllers/pod_reconciler.go
@@ -18,10 +18,14 @@ package controllers
 
 import (
 	"context"
-	"k8s.io/klog/v2"
-
+	"fmt"
+	"github.com/containers/image/v5/docker"
+	"github.com/containers/image/v5/image"
+	"github.com/containers/image/v5/manifest"
+	"github.com/containers/image/v5/types"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/klog/v2"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -32,6 +36,11 @@ type PodReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
 }
+
+// There is not Set implementation in Golang. The void struct is thought to emulate it via maps.
+type void struct{}
+
+var voidVal void
 
 //+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch;update;patch
 //+kubebuilder:rbac:groups=core,resources=pods/status,verbs=get;update;patch
@@ -71,15 +80,12 @@ func (r *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 	// Prepare the requirement for the node affinity.
 	architectureRequirement, err := prepareRequirement(ctx, pod)
 	if err != nil {
-		klog.Errorf("unable to get the architecture requirements for pod %s/%s: %v", pod.Namespace, pod.Name, err)
-		return ctrl.Result{}, err
-	}
-
-	// Update the node affinity
-	err = setPodNodeAffinityRequirement(ctx, pod, architectureRequirement)
-	if err != nil {
-		klog.Errorf("unable to set the node affinity requirement for pod %s/%s: %v", pod.Namespace, pod.Name, err)
-		return ctrl.Result{}, err
+		klog.Errorf("unable to get the architecture requirements for pod %s/%s: %v. "+
+			"The nodeAffinity for this pod will not be set.", pod.Namespace, pod.Name, err)
+		// we still need to remove the scheduling gate. Therefore, we do not return here.
+	} else {
+		// Update the node affinity
+		setPodNodeAffinityRequirement(ctx, pod, architectureRequirement)
 	}
 
 	// Remove the scheduling gate
@@ -96,18 +102,22 @@ func (r *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 }
 
 func prepareRequirement(ctx context.Context, pod *corev1.Pod) (corev1.NodeSelectorRequirement, error) {
-	// TODO: inspect the images in the pod spec to infer the values
+	values, err := inspectImages(ctx, pod)
+	// if an error occurs, we return an empty NodeSelectorRequirement and the error.
+	if err != nil {
+		return corev1.NodeSelectorRequirement{}, err
+	}
 	return corev1.NodeSelectorRequirement{
 		Key:      "kubernetes.io/arch",
 		Operator: corev1.NodeSelectorOpIn,
-		Values:   []string{"amd64", "arm64", "s390x", "ppc64le"},
+		Values:   values,
 	}, nil
 }
 
 // setPodNodeAffinityRequirement sets the node affinity for the pod to the given requirement based on the rules in
 // the sig-scheduling's KEP-3838: https://github.com/kubernetes/enhancements/tree/master/keps/sig-scheduling/3838-pod-mutable-scheduling-directives.
 func setPodNodeAffinityRequirement(ctx context.Context, pod *corev1.Pod,
-	requirement corev1.NodeSelectorRequirement) error {
+	requirement corev1.NodeSelectorRequirement) {
 	// We are ignoring the podSpec.nodeSelector field,
 	// TODO: validate this is ok when a pod has both nodeSelector and (our) nodeAffinity
 	if pod.Spec.Affinity == nil {
@@ -151,7 +161,6 @@ func setPodNodeAffinityRequirement(ctx context.Context, pod *corev1.Pod,
 			nodeSelectorTerms[i].MatchExpressions = append(nodeSelectorTerms[i].MatchExpressions, requirement)
 		}
 	}
-	return nil
 }
 
 func hasSchedulingGate(pod *corev1.Pod) bool {
@@ -180,6 +189,133 @@ func removeSchedulingGate(pod *corev1.Pod) {
 		}
 	}
 	pod.Spec.SchedulingGates = filtered
+}
+
+// inspectImages returns the list of supported architectures for the images used by the pod.
+// if an error occurs, it returns the error and a nil slice of strings.
+func inspectImages(ctx context.Context, pod *corev1.Pod) (supportedArchitectures []string, err error) {
+	// Build a set of all the images used by the pod
+	imageNamesSet := map[string]void{}
+	for _, container := range append(pod.Spec.Containers, pod.Spec.InitContainers...) {
+		imageNamesSet[fmt.Sprintf("//%s", container.Image)] = voidVal
+	}
+	klog.V(3).Infof("Images list for pod %s/%s: %+v", pod.Namespace, pod.Name, imageNamesSet)
+	// https://github.com/containers/skopeo/blob/v1.11.1/cmd/skopeo/inspect.go#L72
+	// Iterate over the images, get their architectures and intersect (as in set intersection) them each other
+	supportedArchitecturesPerImage := make([][]string, 0, len(imageNamesSet))
+	for imageName := range imageNamesSet {
+		currentImageSupportedArchitectures, err := inspectImage(ctx, pod, imageName)
+		if err != nil {
+			// The image cannot be inspected, we skip from adding the nodeAffinity
+			klog.Warningf("Error inspecting the image %s: %v", imageName, err)
+			return nil, err
+		}
+		supportedArchitecturesPerImage = append(supportedArchitecturesPerImage, currentImageSupportedArchitectures)
+	}
+
+	// Intersect the supported architectures of each image once the inspection of all the images is done
+	supportedArchitectures = intersect(supportedArchitecturesPerImage...)
+	return
+}
+
+// inspectImage inspects the image and returns the supported architectures. Any error when inspecting the image is returned so that
+// the caller can decide what to do.
+func inspectImage(ctx context.Context, pod *corev1.Pod, imageName string) (supportedArchitectures []string, err error) {
+	klog.V(5).Infof("Checking %s/%s's image %s", pod.Namespace, pod.Name, imageName)
+	// Check if the image is a manifest list
+	ref, err := docker.ParseReference(imageName)
+	if err != nil {
+		klog.Warningf("Error parsing the image reference for the %s/%s's image %s: %v",
+			pod.Namespace, pod.Name, imageName, err)
+		return nil, err
+	}
+
+	// TODO: handle private registries, credentials, TLS verification, etc.
+	// For OCP, we also need to handle the access to the internal registry via the proper RBAC rule.
+	src, err := ref.NewImageSource(ctx, &types.SystemContext{
+		OCIInsecureSkipTLSVerify:    true,
+		DockerInsecureSkipTLSVerify: types.OptionalBoolTrue,
+	})
+	if err != nil {
+		klog.Warningf("Error creating the image source: %v", err)
+		return nil, err
+	}
+	defer func(src types.ImageSource) {
+		err := src.Close()
+		if err != nil {
+			klog.Warningf("Error closing the image source for the %s/%s's image %s: %v",
+				pod.Namespace, pod.Name, imageName, err)
+		}
+	}(src)
+
+	rawManifest, _, err := src.GetManifest(ctx, nil)
+	if err != nil {
+		klog.Infof("Error getting the image manifest: %v", err)
+		return nil, err
+	}
+	if manifest.MIMETypeIsMultiImage(manifest.GuessMIMEType(rawManifest)) {
+		klog.V(5).Infof("%s/%s's image %s is a manifest list... getting the list of supported architectures",
+			pod.Namespace, pod.Name, imageName)
+		// The image is a manifest list
+		index, err := manifest.OCI1IndexFromManifest(rawManifest)
+		if err != nil {
+			klog.Warningf("Error parsing the OCI index from the raw manifest of the %s/%s's image %s: %v",
+				pod.Namespace, pod.Name, imageName, err)
+		}
+		supportedArchitectures = make([]string, 0, len(index.Manifests))
+		for _, m := range index.Manifests {
+			supportedArchitectures = append(supportedArchitectures, m.Platform.Architecture)
+		}
+		return supportedArchitectures, nil
+
+	} else {
+		klog.V(5).Infof("%s/%s's image %s is not a manifest list... getting the supported architecture",
+			pod.Namespace, pod.Name, imageName)
+		sys := &types.SystemContext{}
+		parsedImage, err := image.FromUnparsedImage(ctx, sys, image.UnparsedInstance(src, nil))
+		if err != nil {
+			klog.Warningf("Error parsing the manifest of the %s/%s's image %s: %v",
+				pod.Namespace, pod.Name, imageName, err)
+			return nil, err
+		}
+		config, err := parsedImage.OCIConfig(ctx)
+		if err != nil {
+			// Ignore errors due to invalid images at this stage
+			klog.Warningf("Error parsing the OCI config of the %s/%s's image %s: %v",
+				pod.Namespace, pod.Name, imageName, err)
+			return nil, err
+		}
+		return []string{config.Architecture}, nil
+	}
+}
+
+func intersect[T comparable](sets ...[]T) []T {
+	setMap := make(map[T]int) // Map to track element occurrences
+
+	// Count occurrences of elements in all sets
+	for _, set := range sets {
+		visited := make(map[T]void) // Track visited elements in the current set
+
+		for _, element := range set {
+			// Skip if the element has already been visited in the current set
+			if _, ok := visited[element]; ok {
+				continue
+			}
+			setMap[element]++
+		}
+	}
+
+	intersection := make([]T, 0, len(setMap))
+	setCount := len(sets)
+
+	// Check for elements that occurred in all sets, considering duplicates
+	for element, occurrences := range setMap {
+		if occurrences == setCount {
+			intersection = append(intersection, element)
+		}
+	}
+
+	return intersection
 }
 
 // SetupWithManager sets up the controller with the Manager.

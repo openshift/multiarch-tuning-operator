dfc1bce46bf5b2c6f3e74074c8e544001835dd49
aleskandro <aleskandro@redhat.com>
2023-07-13 18:19:01 +0200

    SystemConfigSyncer
    The SystemConfigSyncer is a thread-safe singleton object responsible for keeping (1) the registries.conf, (2) the policy.json, and (3) the per-host certificates authority to store in the certs.d docker config folder.
    When instantiated as a singleton, it also registers handlers for:
    1. The openshift-image-registry/image-registry-certificates configmap, storing the per-registry custom certificates authorities
    2. The image.config.openshift.io/cluster object, storing the allowed, blocked and insecure registries
    
    [TODO] The ICSP and IDMS controllers will also use the methods defined in this package to marshall the image registries mirror configuration into the registries.conf files

---
+++ pkg/system_config/interfaces.go
@@ -0,0 +1,16 @@
+package system_config
+
+type IConfigSyncer interface {
+
+	// StoreImageRegistryConf stores the allowedRegistries and blockedRegistries in the structs representing the
+	// registries.conf and policy.json files. It fails if both allowedRegistries and blockedRegistries are set.
+	StoreImageRegistryConf(allowedRegistries []string, blockedRegistries []string, insecureRegistries []string) error
+
+	StoreRegistryCerts(registryCertTuples []registryCertTuple) error
+
+	UpdateRegistryMirroringConfig(registry string, mirrors []string) error
+	DeleteRegistryMirroringConfig(registry string) error
+	CleanupRegistryMirroringConfig() error
+
+	sync() error
+}

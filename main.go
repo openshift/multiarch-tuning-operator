b0573fb36088f8fa9982cb3b10a1b5491582197d
aleskandro <aleskandro@redhat.com>
2023-09-07 21:38:50 +0200

    Let the ctrl.Options.CertDir field be changed at runtime via a flag
    This commit let the CertDir field of the controller options to be changed at runtime via command-line flags. It is useful for debugging purposes and to run the operator in a local, dev, environment

--- main.go
+++ main.go
@@ -68,11 +68,13 @@ func main() {
 	var metricsAddr string
 	var enableLeaderElection bool
 	var probeAddr string
+	var certDir string
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
+	flag.StringVar(&certDir, "cert-dir", "/var/run/manager/tls", "The directory where the TLS certs are stored")
 	opts := zap.Options{
 		Development: true,
 	}
@@ -91,7 +93,7 @@ func main() {
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "208d7abd.multiarch.openshift.io",
-		CertDir:                "/var/run/manager/tls",
+		CertDir:                certDir,
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly

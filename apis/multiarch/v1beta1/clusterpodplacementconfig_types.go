59878f17f25cb209a9468b4f01fc768f31bfb830
AnnaZivkovic <tzivkovi@redhat.com>
2024-06-11 12:33:28 -0700

    MULTIARCH-4545: added new api v1beta1 as hub
    - v1beta1 is the hub and v1alpha1 is a spoke
    - let the sample be for v1beta1
    - update manifests

---
+++ apis/multiarch/v1beta1/clusterpodplacementconfig_types.go
@@ -0,0 +1,91 @@
+/*
+Copyright 2023 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ClusterPodPlacementConfigSpec defines the desired state of ClusterPodPlacementConfig
+type ClusterPodPlacementConfigSpec struct {
+	// LogVerbosity is the log level for the pod placement controller.
+	// Valid values are: "Normal", "Debug", "Trace", "TraceAll".
+	// Defaults to "Normal".
+	// +optional
+	// +kubebuilder:default=Normal
+	LogVerbosity LogVerbosityLevel `json:"logVerbosity,omitempty"`
+
+	// NamespaceSelector filters the namespaces that the architecture aware pod placement can operate.
+	//
+	// For example, users can configure an opt-out filter to disallow the operand from operating on namespaces with a given
+	// label:
+	//
+	// {"namespaceSelector":{"matchExpressions":[{"key":"multiarch.openshift.io/exclude-pod-placement","operator":"DoesNotExist"}]}}
+	//
+	// The operand will set the node affinity requirement in all the pods created in namespaces that do not have
+	// the `multiarch.openshift.io/exclude-pod-placement` label.
+	//
+	// Alternatively, users can configure an opt-in filter to operate only on namespaces with specific labels:
+	//
+	// {"namespaceSelector":{"matchExpressions":[{"key":"multiarch.openshift.io/include-pod-placement","operator":"Exists"}]}}
+	//
+	// The operand will set the node affinity requirement in all the pods created in namespace labeled with the key
+	// `multiarch.ioenshift.io/include-pod-placement`.
+	//
+	// See
+	// https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/
+	// for more examples of label selectors.
+	//
+	// Default to the empty LabelSelector, which matches everything. Selectors are ANDed.
+	// +optional
+	NamespaceSelector *metav1.LabelSelector `json:"namespaceSelector,omitempty"`
+}
+
+// ClusterPodPlacementConfigStatus defines the observed state of ClusterPodPlacementConfig
+type ClusterPodPlacementConfigStatus struct {
+	// Conditions represents the latest available observations of a ClusterPodPlacementConfig's current state.
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
+}
+
+// ClusterPodPlacementConfig is the Schema for the clusterpodplacementconfigs API
+// +kubebuilder:object:root=true
+// +kubebuilder:storageversion
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=clusterpodplacementconfigs,scope=Cluster
+type ClusterPodPlacementConfig struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ClusterPodPlacementConfigSpec   `json:"spec,omitempty"`
+	Status ClusterPodPlacementConfigStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// ClusterPodPlacementConfigList contains a list of ClusterPodPlacementConfig
+type ClusterPodPlacementConfigList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ClusterPodPlacementConfig `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ClusterPodPlacementConfig{}, &ClusterPodPlacementConfigList{})
+}
--- main.go
+++ main.go
@@ -47,6 +47,8 @@ import (
 	"github.com/openshift/library-go/pkg/operator/events"
 
 	multiarchv1alpha1 "github.com/openshift/multiarch-tuning-operator/apis/multiarch/v1alpha1"
+	multiarchv1beta1 "github.com/openshift/multiarch-tuning-operator/apis/multiarch/v1beta1"
+
 	"github.com/openshift/multiarch-tuning-operator/controllers/operator"
 	"github.com/openshift/multiarch-tuning-operator/controllers/podplacement"
 	"github.com/openshift/multiarch-tuning-operator/pkg/utils"
@@ -78,6 +80,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 	utilruntime.Must(multiarchv1alpha1.AddToScheme(scheme))
+	utilruntime.Must(multiarchv1beta1.AddToScheme(scheme))
 
 	// TODO[OCP specific]
 	utilruntime.Must(ocpv1.Install(scheme))
@@ -146,7 +149,7 @@ func main() {
 func RunOperator(mgr ctrl.Manager) {
 	config := ctrl.GetConfigOrDie()
 	clientset := kubernetes.NewForConfigOrDie(config)
-	gvk, _ := apiutil.GVKForObject(&multiarchv1alpha1.ClusterPodPlacementConfig{}, mgr.GetScheme())
+	gvk, _ := apiutil.GVKForObject(&multiarchv1beta1.ClusterPodPlacementConfig{}, mgr.GetScheme())
 	must((&operator.ClusterPodPlacementConfigReconciler{
 		Client:    mgr.GetClient(),
 		Scheme:    mgr.GetScheme(),

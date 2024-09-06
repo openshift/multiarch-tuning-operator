8e4913c97619cd1a2246d0275df7b0aac81cb689
aleskandro <aleskandro@redhat.com>
2024-09-06 11:20:26 -0400

    MULTIARCH-4294: Add metrics package for pod placement

---
+++ controllers/podplacement/metrics/webhook.go
@@ -0,0 +1,46 @@
+package metrics
+
+import (
+	"sync"
+
+	metrics2 "sigs.k8s.io/controller-runtime/pkg/metrics"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	ProcessedPodsWH prometheus.Counter
+	GatedPods       prometheus.Counter
+	ResponseTime    prometheus.Histogram
+)
+
+var onceWebhook sync.Once
+
+func InitWebhookMetrics() {
+	onceWebhook.Do(initWebhookMetrics)
+}
+
+func initWebhookMetrics() {
+	initCommonMetrics()
+	ProcessedPodsWH = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "mto_ppo_wh_pods_processed_total",
+			Help: "The total number of pods processed by the webhook",
+		},
+	)
+	GatedPods = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "mto_ppo_wh_pods_gated_total",
+			Help: "The total number of pods gated by the webhook",
+		},
+	)
+
+	ResponseTime = prometheus.NewHistogram(
+		prometheus.HistogramOpts{
+			Name:    "mto_ppo_wh_response_time_seconds",
+			Help:    "The response time of the webhook",
+			Buckets: buckets(),
+		},
+	)
+	metrics2.Registry.MustRegister(ProcessedPodsWH, GatedPods, ResponseTime)
+}

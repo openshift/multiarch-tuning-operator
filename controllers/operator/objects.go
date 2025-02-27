93a5a4b3cdd5c85b34b4427f7c6b23a90864b7fd
aleskandro <aleskandro@redhat.com>
2025-02-27 16:13:09 +0000

    [4.18] [sig-auth] all workloads in ns/openshift-multiarch-tuning-operator must set the 'openshift.io/required-scc' annotation
    See https://github.com/openshift/enhancements/blob/9b5d8a964fc91e8ace17e373923f1c61cdd1a96f/enhancements/authentication/custom-scc-preemption-prevention.md?plain=1#L100
    
    # Conflicts:
    #	bundle/manifests/multiarch-tuning-operator.clusterserviceversion.yaml

--- controllers/operator/objects.go
+++ controllers/operator/objects.go
@@ -31,6 +31,11 @@ const (
 	serviceAccountKind = "ServiceAccount"
 	roleKind           = "Role"
 	clusterRoleKind    = "ClusterRole"
+
+	// xref: https://github.com/openshift/enhancements/blob/9b5d8a964fc/enhancements/authentication/custom-scc-preemption-prevention.md
+	requiredSCCAnnotation      = "openshift.io/required-scc"
+	requiredSCCRestrictedV2    = "restricted-v2"
+	requiredSCCHostmoundAnyUID = "hostmount-anyuid"
 )
 
 func buildMutatingWebhookConfiguration(clusterPodPlacementConfig *v1beta1.ClusterPodPlacementConfig) *admissionv1.MutatingWebhookConfiguration {
@@ -123,6 +128,10 @@ func buildControllerDeployment(clusterPodPlacementConfig *v1beta1.ClusterPodPlac
 	d := buildDeployment(clusterPodPlacementConfig, utils.PodPlacementControllerName, 2, utils.PodPlacementControllerName,
 		utils.PodPlacementFinalizerName, "--leader-elect", "--enable-ppc-controllers",
 	)
+	if d.Spec.Template.Annotations == nil {
+		d.Spec.Template.Annotations = map[string]string{}
+	}
+	d.Spec.Template.Annotations[requiredSCCAnnotation] = requiredSCCHostmoundAnyUID
 	d.Spec.Template.Spec.Volumes = append(d.Spec.Template.Spec.Volumes,
 		corev1.Volume{
 			Name: "docker-conf",
@@ -198,6 +207,7 @@ func buildDeployment(clusterPodPlacementConfig *v1beta1.ClusterPodPlacementConfi
 					Annotations: map[string]string{
 						// See https://github.com/openshift/enhancements/blob/c5b9aea25e/enhancements/workload-partitioning/management-workload-partitioning.md
 						"target.workload.openshift.io/management": "{\"effect\": \"PreferredDuringScheduling\"}",
+						requiredSCCAnnotation:                     requiredSCCRestrictedV2,
 					},
 				},
 				Spec: corev1.PodSpec{
@@ -301,9 +311,6 @@ func buildDeployment(clusterPodPlacementConfig *v1beta1.ClusterPodPlacementConfi
 								Privileged:             utils.NewPtr(false),
 								ReadOnlyRootFilesystem: utils.NewPtr(true),
 								RunAsNonRoot:           utils.NewPtr(true),
-								SeccompProfile: &corev1.SeccompProfile{
-									Type: corev1.SeccompProfileTypeRuntimeDefault,
-								},
 							},
 							VolumeMounts: []corev1.VolumeMount{
 								{
@@ -323,9 +330,6 @@ func buildDeployment(clusterPodPlacementConfig *v1beta1.ClusterPodPlacementConfi
 					ServiceAccountName: serviceAccount,
 					SecurityContext: &corev1.PodSecurityContext{
 						RunAsNonRoot: utils.NewPtr(true),
-						SeccompProfile: &corev1.SeccompProfile{
-							Type: corev1.SeccompProfileTypeRuntimeDefault,
-						},
 					},
 					TopologySpreadConstraints: []corev1.TopologySpreadConstraint{
 						{

78524ab426759773e58be20b77dc15695628cc1d
AnnaZivkovic <tzivkovi@redhat.com>
2025-03-28 15:27:41 -0700

    Updated the tools in the Makefile to the desired version.
    This required updating the golangci-lint path and running: golangci-lint migrate
    
    https://golangci-lint.run/product/migration-guide/

--- hack/golangci-lint.sh
+++ hack/golangci-lint.sh
@@ -2,5 +2,5 @@
 
 set -eux
 echo "Running golangci-lint..."
-GOFLAGS='' go install "github.com/golangci/golangci-lint/cmd/golangci-lint@${GOLINT_VERSION:-latest}"
+GOFLAGS='' go install "github.com/golangci/golangci-lint/v2/cmd/golangci-lint@${GOLINT_VERSION:-latest}"
 golangci-lint run --timeout 5m0s --verbose --skip-dirs vendor --skip-files zz_generated*

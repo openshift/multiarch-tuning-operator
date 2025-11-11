564ca0bf2ef32aa21c18b1cb6cdd9e0b7cde1226
lwan <lwan@redhat.com>
2025-11-11 12:36:54 +0800

    Adding bundle.Dockerfile changes

--- hack/bump-version.sh
+++ hack/bump-version.sh
@@ -17,10 +17,23 @@ echo "Bumping version to: $VERSION"
 yq -i ".spec.version=\"${VERSION}\"" config/manifests/bases/multiarch-tuning-operator.clusterserviceversion.yaml
 yq -i ".metadata.name=\"multiarch-tuning-operator.v${VERSION}\"" config/manifests/bases/multiarch-tuning-operator.clusterserviceversion.yaml
 yq -i ".spec.startingCSV=\"multiarch-tuning-operator.v${VERSION}\"" deploy/base/operators.coreos.com/subscriptions/openshift-multiarch-tuning-operator/subscription.yaml
-yq -i ".entries[0].name=\"multiarch-tuning-operator.v${VERSION}\"" index.base.yaml
-sed -i "s/^LABEL release=.*/LABEL release=\"${VERSION}\"/" Dockerfile
-sed -i "s/^LABEL version=.*/LABEL version=\"${VERSION}\"/" Dockerfile
-sed -i "s/^LABEL release=.*/LABEL release=\"${VERSION}\"/" konflux.Dockerfile
-sed -i "s/^LABEL version=.*/LABEL version=\"${VERSION}\"/" konflux.Dockerfile
-sed -i "s/^VERSION ?= .*/VERSION ?= ${VERSION}/" Makefile
+yq eval-all -i "(select(.schema==\"olm.channel\").entries[0].name)=\"multiarch-tuning-operator.v${VERSION}\"" index.base.yaml
+
+
+if [[ "$(uname)" == "Darwin" ]]; then
+    # macOS BSD sed
+    sed -i '' "s/^LABEL release=.*/LABEL release=\"${VERSION}\"/" Dockerfile
+    sed -i '' "s/^LABEL version=.*/LABEL version=\"${VERSION}\"/" Dockerfile
+    sed -i '' "s/^LABEL release=.*/LABEL release=\"${VERSION}\"/" konflux.Dockerfile
+    sed -i '' "s/^LABEL version=.*/LABEL version=\"${VERSION}\"/" konflux.Dockerfile
+    sed -i '' "s/^VERSION ?= .*/VERSION ?= ${VERSION}/" Makefile
+else
+    # Linux GNU sed
+    sed -i "s/^LABEL release=.*/LABEL release=\"${VERSION}\"/" Dockerfile
+    sed -i "s/^LABEL version=.*/LABEL version=\"${VERSION}\"/" Dockerfile
+    sed -i "s/^LABEL release=.*/LABEL release=\"${VERSION}\"/" konflux.Dockerfile
+    sed -i "s/^LABEL version=.*/LABEL version=\"${VERSION}\"/" konflux.Dockerfile
+    sed -i "s/^VERSION ?= .*/VERSION ?= ${VERSION}/" Makefile
+fi
+echo "make bundle"
 make bundle
